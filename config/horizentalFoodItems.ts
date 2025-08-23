005ffd2d848ddcef944f8d5f9ada7d6cd49a1e35
Hilal <140879548+Hilal-11@users.noreply.github.com>
2025-08-23 23:45:38 +0530

    config static daa

---
+++ config/horizentalFoodItems.ts
@@ -0,0 +1,48 @@
+
+interface FoodItemsConfig {
+    id: string,
+    name: string,
+    image: string
+}
+export const foodItemsData: FoodItemsConfig[] = [
+  {
+    "id": "1",
+    "name": "Burger",
+    "image": "https://cdn.pixabay.com/photo/2016/03/05/19/02/hamburger-1238246_1280.jpg"
+  },
+  {
+    "id": "2",
+    "name": "Pizza",
+    "image": "https://cdn.pixabay.com/photo/2017/12/09/08/18/pizza-3007395_1280.jpg"
+  },
+  {
+    "id": "3",
+    "name": "French Fries",
+    "image": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRSYXoSG0NoDgywh1JMG9AmoH9QOdv6dRSF12Kagib9hY4hP_BIdO9YKKV4z6QxD2yNYUQ&usqp=CAU"
+  },
+  {
+    "id": "4",
+    "name": "Hot Dog",
+    "image": "https://thumbs.dreamstime.com/b/french-fries-fast-food-restaurant-falling-disrupted-isolate-white-background-331616830.jpg"
+  },
+  {
+    "id": "5",
+    "name": "Sandwich",
+    "image": "https://images.immediate.co.uk/production/volatile/sites/30/2013/05/2022-09-23GFOWEBFamilyRecipes-OnePotGarlicChicken05875preview-d8a4a42.jpg"
+  },
+  {
+    "id": "6",
+    "name": "Taco",
+    "image": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQHw90N08zWPPharTf5PliW2Ff1oLCg7IoUImMLWsgFQdt9eHCMCDArp8MV1dejrEzXjdY&usqp=CAU"
+  },
+  {
+    "id": "7",
+    "name": "Fried Chicken",
+    "image": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRSYXoSG0NoDgywh1JMG9AmoH9QOdv6dRSF12Kagib9hY4hP_BIdO9YKKV4z6QxD2yNYUQ&usqp=CAU"
+  },
+  {
+    "id": "8",
+    "name": "Donut",
+    "image": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRSYXoSG0NoDgywh1JMG9AmoH9QOdv6dRSF12Kagib9hY4hP_BIdO9YKKV4z6QxD2yNYUQ&usqp=CAU"
+  }
+]
